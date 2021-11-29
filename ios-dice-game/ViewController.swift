4590a9236d0a2c8173a639b9eca5be9f9472b64a
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-11-29 22:31:16 +0900

    Initial commit

---
+++ ios-dice-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-dice-game
+//
+//  Created by 이성노 on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
