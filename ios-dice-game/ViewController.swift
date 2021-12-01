5496eb201268ea890c942195714eff5f84551621
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-12-01 22:52:45 +0900

    feat: diceArray 생성
    #imageLiteral( 를 이용한 diceArray 생성

--- ios-dice-game/ViewController.swift
+++ ios-dice-game/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstImageView: UIImageView!
     @IBOutlet weak var secondImageView: UIImageView!
     
-    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
     
     
     
@@ -24,6 +24,9 @@ class ViewController: UIViewController {
     }
 
     
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+    }
     
     
     
