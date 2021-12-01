563652d26d3c4a6693be4710472553034c4e9901
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-12-01 22:56:00 +0900

    feat: secondImageView 랜덤 이미지 설정
    secondImageView 랜덤 이미지 설정

--- ios-dice-game/ViewController.swift
+++ ios-dice-game/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     
     @IBAction func rollButtonTapped(_ sender: UIButton) {
         firstImageView.image = diceArray.randomElement()
+        secondImageView.image = diceArray.randomElement()
     }
     
     
